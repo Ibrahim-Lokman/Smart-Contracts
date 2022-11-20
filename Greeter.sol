55724fa1c81fdf7d1ac4ef6161be91e877126a7c
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2022-11-20 16:37:05 +0600

    first commit

---
+++ Greeter.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.0;
+
+contract Greeter {
+    string public yourName;
+
+    function Greeter() public {
+        yourName = "world";
+    }
+
+    function set(string name) public {
+        yourName = name;
+
+    }
+
+    function hello() constant public returns (string) {
+        return yourName;
+    }
+}
